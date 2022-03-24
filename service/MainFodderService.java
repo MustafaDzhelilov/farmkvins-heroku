35751f927b6bdcd88cc61f200b5d2ea2ab22326e
Mustafa Dzhelilov <82335877+MustafaDzhelilov@users.noreply.github.com>
2022-03-24 22:51:22 +0200

    Delete service directory

--- service/MainFodderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectsoftuni.service;
-
-import com.example.projectsoftuni.model.service.MainFodderServiceModel;
-
-public interface MainFodderService {
-    void add(MainFodderServiceModel mainFodderServiceModel);
-
-    Long getKg();
-}
